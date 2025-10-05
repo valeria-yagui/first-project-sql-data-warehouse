137eefad5bbbaa076c11bd8891a1f55f97cb3b05
Valeria Yagui Nishii <132927144+valeria-yagui@users.noreply.github.com>
2025-10-06 00:04:55 +0200

    Adding illustrations and scripts

---
+++ scripts/1.Create_database.sql
@@ -0,0 +1,21 @@
+/*
+=============================================================================================
+Script Purpose:
+	- This script creates the data base 'DataWarehouse' and schemas 'bronze', 'silver', 'gold'.
+=============================================================================================
+*/
+
+USE master;
+
+CREATE DATABASE DataWarehouse
+
+USE DataWarehouse;
+
+-- We create the SCHEMAS that are a logical collention of database objects like tables or views.
+CREATE SCHEMA bronze;
+GO  -- It's a separator. Tells SQL to completely execute the first command before going to the next one.
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
+
